9c27a668b43dc873f3983a63578524eec9a51db6
Omprakash Choudhary <omseervi098@gmail.com>
2022-08-04 21:29:45 +0530

    update

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 const port = 3000;
 //Use express Router
 app.use("/", require("./routes/index"));
+//Setting up the view engine
+app.set('view engine', 'ejs');
+app.set('views','./views');
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error : ${err}`);
