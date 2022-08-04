6cc914255d2fbc1899400b3e0ff1c7db5ef7fde4
Omprakash Choudhary <omseervi098@gmail.com>
2022-08-04 21:26:11 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const homeController=require('../controllers/home_controller');
 
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
+//For any further routes, acces from here:
+//router.use('/routerName',require('./routerFile'));
 
 module.exports=router;
