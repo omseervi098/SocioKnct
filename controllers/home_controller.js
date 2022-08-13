19871f79f7d4b10fcac18283ad3c238321d1c89a
Omprakash Choudhary <omseervi098@gmail.com>
2022-08-13 13:30:06 +0530

    Completed Comment section

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,14 @@
 const Post=require('../models/post');
 module.exports.home=function(req,res){
-    Post.find({}).populate('user').exec((err,posts)=>{
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec((err,posts)=>{
         if(err){
             return console.log(err);
         }
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express=require('express');
 const router=express.Router();
 const homeController=require('../controllers/home_controller');
 
+
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
-
+router.use('/comments',require('./comments'));
 //For any further routes, acces from here:
 //router.use('/routerName',require('./routerFile'));
 
