6ae5785da74c66cb2d00bf7bc3d25e308346605a
Omprakash Choudhary <omseervi098@gmail.com>
2022-08-13 11:26:03 +0530

    displaying user and its post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,9 @@
 const Post=require('../models/post');
 module.exports.home=function(req,res){
-    Post.find({},(err,posts)=>{
+    Post.find({}).populate('user').exec((err,posts)=>{
+        if(err){
+            return console.log(err);
+        }
         return res.render('home',{
             title:'Home',
             posts:posts
