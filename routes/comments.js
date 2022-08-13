3dee5f3512e5609eb3b52a47b9a8dedd00c7a479
Omprakash Choudhary <omseervi098@gmail.com>
2022-08-13 12:51:49 +0530

    deleted:    controllers/comments_controller.js 	deleted:    routes/comments.js 	modified:   routes/posts.js

--- routes/comments.js
+++
@@ -1,6 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const passport=require('passport');
-const commentController=require('../controllers/comments_controller');
-router.post('/create',passport.checkAuthenication,commentController.create);
-module.exports=router;
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
-
+const passport=require('passport');
 
 const postsController=require('../controllers/posts_controller');
 
