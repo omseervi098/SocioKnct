0f41496db48b382cf44b19106ee2339042def38f
Omprakash Choudhary <omseervi098@gmail.com>
2022-08-13 12:51:25 +0530

    Revert "checking authenication while creating post"
    This reverts commit 3feaf8e63876e6bc64fbb15666e45d9d36573c5a.
    
    	modified:   routes/posts.js
    	modified:   views/home.ejs
    	deleted:    controllers/comments_controller.js
    	deleted:    routes/comments.js
    	modified:   routes/posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,8 @@
 const express=require('express');
 const router=express.Router();
-const passport=require('passport');
+
 
 const postsController=require('../controllers/posts_controller');
 
 router.post('/create',passport.checkAuthenication,postsController.create);
-
 module.exports=router;
