0dbfdb8e00f6cff022e95d2d72d7e1ea26305d1e
Omprakash Choudhary <omseervi098@gmail.com>
2022-08-14 07:13:08 +0530

    added delete post feature

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post');
+const Comment=require('../models/comment');
 module.exports.create=(req,res)=>{
     Post.create({
         content:req.body.content,
@@ -8,5 +9,20 @@ module.exports.create=(req,res)=>{
             return console.log(err);
         }
         return res.redirect('/');
+    })
+}
+
+module.exports.destroy=(req,res)=>{
+    Post.findById(req.params.id,(err,post)=>{
+        //.id means converting object into string
+        if(post.user=req.user.id){
+            post.remove();
+            
+            Comment.deleteMany({post:req.params.id},(err)=>{
+                return res.redirect('/');
+            })
+        }else{
+            return res.redirect('/');  
+        }
     })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -5,4 +5,5 @@ const passport=require('passport');
 const postsController=require('../controllers/posts_controller');
 
 router.post('/create',passport.checkAuthenication,postsController.create);
+router.get('/destroy/:id',passport.checkAuthenication,postsController.destroy);
 module.exports=router;
