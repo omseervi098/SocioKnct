a08b17c4dce05cfd49c47d238217314eaa79039b
Omprakash Choudhary <omseervi098@gmail.com>
2022-12-27 16:19:58 +0530

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 const env = require("./environment");
 //check if database exists
-mongoose.connect(`mongodb://localhost:27017/${env.db}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(`mongodb://localhost:27017`);
+//now check if the database exists
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
