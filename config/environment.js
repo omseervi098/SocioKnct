96b55ccc4ec2fe5ff9a46b251d3bd47e88cd9f64
Omprakash Choudhary <omseervi098@gmail.com>
2023-06-27 21:01:51 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -37,6 +37,8 @@ const production = {
   asset_path: process.env.CODEIAL_ASSET_PATH,
   session_cookie_key: process.env.CODEIAL_SESSION_COOKIE_KEY,
   db: process.env.CODEIAL_DB_NAME,
+  port:process.env.PORT,
+  socketport:process.env.SOCKETPORT,
   smtp: {
     pool: true,
     host: "smtp.socioknct.tech",
