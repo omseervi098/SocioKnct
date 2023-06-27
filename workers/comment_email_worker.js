9a243cd24f62f301f01d6e6325dae94252b6d29f
Omprakash Choudhary <omseervi098@gmail.com>
2023-06-27 21:19:50 +0530

    Update comment_email_worker.js

--- workers/comment_email_worker.js
+++ workers/comment_email_worker.js
@@ -1,7 +1,7 @@
 const Queue = require('bull');
 const env=require('../config/environment');
 const commentsMailer=require('../mailers/comments_mailer')
-const emailQueue=new Queue('emails',`redis://default:${env.redis_password}@redis-14455.c305.ap-south-1-1.ec2.cloud.redislabs.com:14455`);
+const emailQueue=new Queue('emails',`redis://default:${env.redis_password}@redis-16171.c301.ap-south-1-1.ec2.cloud.redislabs.com:16171`);
 emailQueue.process(function(job,done){
     console.log('emails worker is processing a job',job.id);
     commentsMailer.newComment(job.data);
